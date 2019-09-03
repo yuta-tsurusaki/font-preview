dab45a44c4e7bee1b9f7c3111919e1dd1573eaa0
yuta-tsurusaki <y.tsurusaki@prog-8.com>
2019-09-03 13:47:22 +0900

    done

--- src/code.ts
+++ src/code.ts
@@ -1,49 +1,66 @@
 async function initData() {
-	const selection = figma.currentPage.selection;
-	if (selection.length == 0 || selection[0].type != 'TEXT') {
-		alert('Please select a text node')
-		figma.closePlugin()
-	}
-	let fontList = [];
-	let fontNames = [];
-	const availableFonts = await figma.listAvailableFontsAsync();
-	availableFonts.forEach(font => {
-		const { family, style } = font.fontName
-		if (fontNames.includes(family)) {
-			const index = fontNames.indexOf(family);
-			fontList[index].styles.push(style);
-		} else {
-			fontNames.push(family);
-			fontList.push({
-				family,
-				styles: [style],
-			});
-		}
-	})
+    const selection = figma.currentPage.selection;
+    if (selection.length == 0 || selection[0].type != 'TEXT') {
+        alert('Please select a text node');
+        figma.closePlugin();
+    }
+    let fontList = [];
+    let fontNames = [];
+    const availableFonts = await figma.listAvailableFontsAsync();
+    availableFonts.forEach(font => {
+        const { family, style } = font.fontName;
+        if (fontNames.includes(family)) {
+            const index = fontNames.indexOf(family);
+            fontList[index].styles.push(style);
+        } else if (!family.match(/\./)) {
+            fontNames.push(family);
+            fontList.push({
+                family,
+                styles: [style],
+            });
+        }
+    });
 
     return { fontList };
 }
 
 initData().then(data => {
-	figma.showUI(__html__, { width: 320, height: 480 });
-	const { fontList } = data;
-	const selection = figma.currentPage.selection;
+    figma.showUI(__html__, { width: 320, height: 480 });
+    const { fontList } = data;
+    const selection = figma.currentPage.selection;
     figma.ui.onmessage = pluginMessage => {
-		switch (pluginMessage.type) {
-			case 'name':
-				figma.ui.postMessage({ pluginMessage: { type: 'nameList', data: { fontList } } })
-				break;
-			case 'change':
-				const text = selection[0] as TextNode;
-				const textLength = text.characters.length;
-				const { fontFamily, fontStyles } = pluginMessage;
-				const fontStyle = fontStyles.includes('Regular') ? 'Regular' : fontStyles[0];
-				figma.loadFontAsync({ family: fontFamily, style: fontStyles[0] })
-					.then(() => { text.setRangeFontName(0, textLength, { family: fontFamily, style: fontStyle }) })
-					.catch(() => { text.setRangeFontName(0, textLength, { family: fontFamily, style: fontStyles[0] }) })
-				break;
-			default:
-				break;
+        switch (pluginMessage.type) {
+            case 'name':
+                figma.ui.postMessage({
+                    pluginMessage: { type: 'nameList', data: { fontList } },
+                });
+                break;
+            case 'change':
+                const text = selection[0] as TextNode;
+                const textLength = text.characters.length;
+                const { fontFamily, fontStyles } = pluginMessage;
+                const fontStyle = fontStyles.includes('Regular')
+                    ? 'Regular'
+                    : fontStyles[0];
+                figma
+                    .loadFontAsync({ family: fontFamily, style: fontStyles[0] })
+                    .then(() => {
+                        text.setRangeFontName(0, textLength, {
+                            family: fontFamily,
+                            style: fontStyle,
+                        });
+                    })
+                    .catch(() => {
+                        text.setRangeFontName(0, textLength, {
+                            family: fontFamily,
+                            style: fontStyles[0],
+                        });
+                    });
+                break;
+            case 'close':
+                figma.closePlugin();
+            default:
+                break;
         }
     };
 });
--- src/ui.tsx
+++ src/ui.tsx
@@ -1,64 +1,132 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import './ui.css'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import './ui.css';
 
-declare function require(path: string): any
+declare function require(path: string): any;
 
 type Props = {};
 type States = {
-    nameList: { family: string, styles: string[] };
+    nameList: { family: string; styles: string[] }[];
+    pinnedFont: { family: string; styles: string[] };
 };
 
 class App extends React.Component<Props, States> {
     constructor(props) {
         super(props);
-        this.fetchFontList()
-        this.state = { nameList: {family:'', styles:[]}}
+        this.fetchFontList();
+        this.state = { nameList: [], pinnedFont: { family: '', styles: [] } };
     }
 
     fetchFontList() {
-        parent.postMessage({pluginMessage: { type: 'name' }}, '*');
+        parent.postMessage({ pluginMessage: { type: 'name' } }, '*');
     }
 
-    changeFont(fontFamily, fontStyles) {
-        parent.postMessage({ pluginMessage: {type: 'change', fontFamily, fontStyles}}, '*')
+    sendFont(font) {
+        parent.postMessage(
+            {
+                pluginMessage: {
+                    type: 'change',
+                    fontFamily: font.family,
+                    fontStyles: font.styles,
+                },
+            },
+            '*'
+        );
+    }
+
+    pinFont(font) {
+        this.setState({ pinnedFont: font });
+        console.log(this.state.pinnedFont);
+    }
+
+    sendPinnedFont() {
+        const { family, styles } = this.state.pinnedFont;
+        parent.postMessage(
+            {
+                pluginMessage: {
+                    type: 'change',
+                    fontFamily: family,
+                    fontStyles: styles,
+                },
+            },
+            '*'
+        );
+    }
+
+    closePlugin() {
+        parent.postMessage({ pluginMessage: { type: 'close' } }, '*');
     }
 
     render() {
         onmessage = event => {
-            const {pluginMessage} = event.data.pluginMessage;
-            switch(pluginMessage.type) {
+            const { pluginMessage } = event.data.pluginMessage;
+            switch (pluginMessage.type) {
                 case 'nameList':
-                    const {fontList} = pluginMessage.data;
+                    const { fontList } = pluginMessage.data;
                     const nameList = fontList.map(font => {
                         return (
-                            <li key={font.family} className="selector-font" onMouseOver={() => this.changeFont(font.family, font.styles)}>{font.family}</li>
-                        )
-                    })
-                    this.setState({nameList})
+                            <li
+                                key={font.family}
+                                className="selector-font"
+                                onMouseOver={() => this.sendFont(font)}
+                                onClick={e => {
+                                    this.pinFont(font);
+                                }}
+                            >
+                                {font.family}
+                            </li>
+                        );
+                    });
+                    this.setState({ nameList });
                     break;
             }
-        }
+        };
         return (
             <div>
-            <div className="main-container">
-                <div className="nav-container">
-                    <div className="nav-menu-wrapper">
-                        <div id="name" className="nav-menu">Name</div>
-                        <div id="style" className="nav-menu">Style</div>
-                        <div id="size" className="nav-menu">Size</div>
+                <div className="main-container">
+                    <div className="nav-container">
+                        <div className="nav-menu-wrapper">
+                            <div id="name" className="nav-menu">
+                                Name
+                            </div>
+                            <div id="style" className="nav-menu">
+                                Style
+                            </div>
+                            <div id="size" className="nav-menu">
+                                Size
+                            </div>
+                        </div>
+                        <button
+                            className="btn-apply"
+                            onClick={() => {
+                                this.closePlugin();
+                            }}
+                        >
+                            Apply
+                        </button>
                     </div>
-                    <button className="btn-apply">Apply</button> 
-                </div>
                     <div className="selector-container">
-                        <ul>
+                        <div className="search-bar">
+                            <input
+                                type="text"
+                                placeholder="Search"
+                                onChange={e => {
+                                    console.log(e.target.value);
+                                }}
+                            />
+                        </div>
+                        <ul
+                            onMouseOut={() => {
+                                this.sendPinnedFont();
+                            }}
+                        >
                             {this.state.nameList}
                         </ul>
                     </div>
-            </div>
+                </div>
             </div>
         );
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('react-page'))
+ReactDOM.render(<App />, document.getElementById('react-page'));
